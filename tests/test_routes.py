1a1d800f25f036466e055feacfcc7fea90fbcf8d
bashgame <amen.kester@gmail.com>
2023-01-28 19:45:56 -0500

    Temporary push
    Want to continue working from a different workstation

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,13 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+
+    def test_get_all_accounts(self):
+        """It should return a list with all accounts"""
+        response = self.client.get(
+            BASE_URL
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.message, [])
+
+        
